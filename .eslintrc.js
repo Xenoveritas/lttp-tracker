ef587dd361c5bd0944cf038b293273bb29d65cc0
Xenoveritas <dmpotter44@gmail.com>
2020-07-23 01:32:59 -0400

    Update lint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,29 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
   ],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
   ],
+  rules: {
+    'no-unused-vars': [ 'error', { 'argsIgnorePattern': '^_' } ]
+  },
+  overrides: [
+    {
+      files: [ '*.ts' ],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+      ]
+    },
+    {
+      files: [ '*.js' ],
+      env: {
+        'browser': false,
+        'node': true,
+        'es6': true
+      }
+    }
+  ]
 };
